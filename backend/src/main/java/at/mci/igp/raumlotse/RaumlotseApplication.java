c39a709498bae313f6dba2104ee9b2e2d4ee1135
Crimson8230 <pernthaler.hannes@gmail.com>
2026-09-11 10:43:20 +0200

    setting up spring-boot

---
+++ backend/src/main/java/at/mci/igp/raumlotse/RaumlotseApplication.java
@@ -0,0 +1,13 @@
+package at.mci.igp.raumlotse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaumlotseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaumlotseApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/at/mci/igp/raumlotse/RaumlotseApplicationTests.java
@@ -0,0 +1,13 @@
+package at.mci.igp.raumlotse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaumlotseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
